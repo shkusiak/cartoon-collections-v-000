d716d6b16cd8bad8759e7440b19f14fcc0c95293
Shea Kusiak <shkusiak@gmail.com>
2018-02-19 18:55:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index {|name, index|
-    puts "#{index}. #{name}"
+    puts "#{index + 1}. #{name}"
   }
 end
 
